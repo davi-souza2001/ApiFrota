16aee4cac4cb1f9650398bca2227247654afa303
davi-souza2001 <davisouza2001dv@gmail.com>
2021-09-09 19:45:07 -0300

    console api work

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,24 +1,34 @@
+function show(car){
+    let output = '';
+
+    output += `<ul><li>${car.id}</li></ul>`;
+    output += `<ul><li>${car.name}</li></ul>`;
+    output += `<ul><li>${car.price}</li></ul>`;
 
 
-async function getcontent(){
+    document.querySelector('.contenttable').innerHTML = output;
+    
+    console.log(car.id)
+    console.log(car.name)
+    console.log(car.price)
+}
+
+async function getApi(){
     try {
-        const response = await fetch('http://localhost:4567/');
+        let list = '';
+        document.querySelector('.contenttable').innerHTML = list;
+        const response = await fetch('http://localhost:3001/cars');
         const data = await response.json();
-
-        show(data)
+        data.map( (cars) => {
+           console.log(cars.id);
+           console.log(cars.name);
+           console.log(cars.price);
+        })
+        
     } catch (error) {
         console.error(error);
     }
 }
 
-getcontent();
+getApi()
 
-function show(users) {
-    let output = '';
-
-    for( let user of users ) {
-        output += `<li>${user.name}</li>`; 
-    }
-
-    document.querySelector('.table').innerHTML = output;
-}
