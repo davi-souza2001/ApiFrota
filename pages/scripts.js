f975c434a84dbae19a0b0d9e41c9b5bcf00e4f92
Paloma <palomaalves@hotmail.com>
2021-09-10 13:58:38 -0300

    fix: API Data request

--- pages/scripts.js
+++ pages/scripts.js
@@ -1,49 +1,77 @@
-/* function show(car){
-    let output = '';
-
-    output += `<ul><li>${car.id}</li></ul>`;
-    output += `<ul><li>${car.name}</li></ul>`;
-    output += `<ul><li>${car.price}</li></ul>`;
-
-
-    document.querySelector('.contenttable').innerHTML = output;
-    
-    console.log(car.id)
-    console.log(car.name)
-    console.log(car.price)
-} */
-
-async function getApi(){
-    try {
-        const response = await fetch('http://localhost:3001/cars');
-        const data = await response.json();
-        let cars = data.map( (cars, i) => {
-            return ` 
-            <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nome</th>
-                    <th>Preço</th>
-                </tr>
-            </thead>
-            <tbody>  
-                <tr>
-                    <td>${i + 1}</td>
-                    <td>${cars.name}</td>
-                    <td>${cars.price}</td> 
-                </tr>
-            <tbody> 
-            </table>
-            `
-        });
-        document.querySelector('.contenttable').innerHTML = cars;
-    } catch (error) {
-        console.error(error);
-    }
+async function getApiData() {
+  try {
+    const response = await fetch("http://frota-api.glitch.me/dados");
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+function insertContent(data) {
+  const preContent = `<small><table>
+            <tr>
+            <th>id</th>
+            <th>timestamp</th>
+            <th>bluetooth</th>
+            <th>Car</th>
+            <th>connectObd</th>
+            <th>Lat</th>
+            <th>Long</th>
+            <th>egnitionStatus</th>
+            <th>elmVersion</th>
+            <th>ethanol %</th>
+            <th>fuelLevel</th>
+            <th>odometer</th>
+            <th>rpm</th>
+            <th>speed</th>
+            <th>throttlePos</th>
+        </tr>`;
 
+  const postContent = `</table></small>`;
 
-getApi()
+  let content = "";
 
+  let cars = data.map((car, i) => {
+    const {
+      id,
+      collectTimestamp,
+      bluetoothIsEnable,
+      carDescription,
+      connectObd,
+      currentLatitude,
+      currentLongitude,
+      ignitionStatus,
+      elmVersion,
+      ethanolPercent,
+      fuelLevel,
+      odometer,
+      rpm,
+      speed,
+      throttlePos,
+    } = car;
+
+    content += ` 
+    <tr>
+        <td>${id}</td>
+        <td>${collectTimestamp}</td>
+        <td>${bluetoothIsEnable}</td>
+        <td>${carDescription}</td>
+        <td>${connectObd}</td>
+        <td>${currentLatitude}</td>
+        <td>${currentLongitude}</td>
+        <td>${ignitionStatus}</td>
+        <td>${elmVersion}</td>
+        <td>${ethanolPercent.value}</td>
+        <td>${fuelLevel}</td>
+        <td>${odometer.value}</td>
+        <td>${rpm.value}</td>
+        <td>${speed.value}</td>
+        <td>${throttlePos.value}</td>
+        </tr>`;
+  });
+  content = preContent + content + postContent;
+  document.querySelector(".contenttable").innerHTML = content;
+}
+
+const apiResponse = await getApiData();
+insertContent(apiResponse);
